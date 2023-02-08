cddddf8839c30e8a953ff6adc3fe299031127f43
Sajib Bin Mamun <sajib15-3435@diu.edu.bd>
2023-02-08 20:46:50 +0600

    Error activity: intent apply in gobackbtn

--- app/src/main/java/com/example/quizo/ErrorActivity.kt
+++ app/src/main/java/com/example/quizo/ErrorActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.quizo.databinding.ActivityErrorBinding
 
 class ErrorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val binding:ActivityErrorBinding
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_error)
+        binding= ActivityErrorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gobackbtn.setOnClickListener(View.OnClickListener {
+
+            val intent=Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        })
+
+
     }
 }
